f45fe841226870d4030bf885df3d9d9dc6dc7000
shervinox <shervin@mycujoo.tv>
2020-04-21 15:15:00 +0200

    Update PlayerWidget with abstraction

--- mls/src/main/java/tv/mycujoo/mls/widgets/PlayerWidget.kt
+++ mls/src/main/java/tv/mycujoo/mls/widgets/PlayerWidget.kt
@@ -6,10 +6,12 @@ import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.PlayerControlView
 import com.google.android.exoplayer2.ui.PlayerView
+import tv.mycujoo.mls.model.AnnotationBundle
 
-class PlayerWidget : ConstraintLayout {
+class PlayerWidget : ConstraintLayout, PlayerWidgetInterface {
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -33,36 +35,35 @@ class PlayerWidget : ConstraintLayout {
 
     private fun init(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) {
 
+        playerControlView = PlayerControlView(context, attrs, defStyleAttr)
+        playerControlView.id = View.generateViewId()
+        addView(playerControlView)
+
+
         playerView = PlayerView(context, attrs, defStyleAttr)
         playerView.id = View.generateViewId()
         playerView.useController = false
-
+        playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH
         addView(playerView)
+
 
         val constraintSet = ConstraintSet()
         constraintSet.clone(this)
+
+        constraintSet.connect(playerControlView.id, ConstraintSet.START, id, ConstraintSet.START)
+        constraintSet.connect(playerControlView.id, ConstraintSet.END, id, ConstraintSet.END)
+        constraintSet.connect(playerControlView.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
+
         constraintSet.connect(playerView.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
         constraintSet.connect(playerView.id, ConstraintSet.START, id, ConstraintSet.START)
         constraintSet.connect(playerView.id, ConstraintSet.END, id, ConstraintSet.END)
         constraintSet.connect(playerView.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
-        constraintSet.constrainHeight(playerView.id, 0)
+        constraintSet.constrainWidth(playerView.id, 0)
 
-        playerControlView = PlayerControlView(context, attrs, defStyleAttr)
-        playerControlView.id = View.generateViewId()
-
-        addView(playerControlView)
-
-        constraintSet.clone(this)
-        constraintSet.connect(
-            playerControlView.id,
-            ConstraintSet.TOP,
-            playerView.id,
-            ConstraintSet.BOTTOM
-        )
-        constraintSet.connect(playerControlView.id, ConstraintSet.START, id, ConstraintSet.START)
-        constraintSet.connect(playerControlView.id, ConstraintSet.END, id, ConstraintSet.END)
 
         constraintSet.applyTo(this)
+
+        playerControlView.bringToFront()
 
     }
 
@@ -71,5 +72,18 @@ class PlayerWidget : ConstraintLayout {
         playerControlView.player = player
         playerControlView.showTimeoutMs = 0
 
+    }
+
+    fun displayAnnotation(annotationBundle: AnnotationBundle) {
+        println("displayAnnotation $annotationBundle")
+    }
+
+    override fun setPlayerControllerState(state: Boolean) {
+        if (state) {
+            playerControlView.visibility = View.VISIBLE
+        } else {
+            playerControlView.visibility = View.GONE
+
+        }
     }
 }
