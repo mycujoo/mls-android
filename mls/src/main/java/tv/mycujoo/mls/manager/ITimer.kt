a5c229f0c1f659ef14f1a15a37e224d460e66a9a
shervinox <shervin@mycujoo.tv>
2020-12-30 16:43:52 +0100

    Rename argument

--- mls/src/main/java/tv/mycujoo/mls/manager/ITimer.kt
+++ mls/src/main/java/tv/mycujoo/mls/manager/ITimer.kt
@@ -3,7 +3,7 @@ package tv.mycujoo.mls.manager
 interface ITimer {
 
     fun getTime(): String
-    fun start(statTimer: TimerEntity.StartTimer, now: Long)
+    fun start(startTimer: TimerEntity.StartTimer, now: Long)
     fun pause(pauseTimer: TimerEntity.PauseTimer, now: Long)
     fun adjust(adjustTimer: TimerEntity.AdjustTimer, now: Long)
     fun skip(skipTimer: TimerEntity.SkipTimer, now: Long)
