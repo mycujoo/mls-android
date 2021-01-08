f30f6c7179337836c957a829eaa28cad719a460b
shervinox <shervin@mycujoo.tv>
2021-01-08 19:47:51 +0100

    Fix null remote-media-client bug

--- cast/src/main/java/tv/mycujoo/mls/cast/CasterSession.kt
+++ cast/src/main/java/tv/mycujoo/mls/cast/CasterSession.kt
@@ -5,8 +5,8 @@ import com.google.android.gms.cast.framework.CastSession
 class CasterSession : ICasterSession {
     var castSession: CastSession? = null
     override fun getRemoteMediaClient(): IRemoteMediaClient? {
-        castSession?.let {
-            return RemoteMediaClient(it.remoteMediaClient)
+        castSession?.remoteMediaClient?.let {
+            return RemoteMediaClient(it)
         }
         return null
     }
