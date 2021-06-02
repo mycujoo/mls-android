0a439faa0b8fe09c8018e33153e1adef04534a21
Shervinox <shervin@mycujoo.tv>
2021-06-02 11:44:17 +0200

    Document IMA

--- ima/src/main/java/tv/mycujoo/mcls/ima/IParamProvider.kt
+++ ima/src/main/java/tv/mycujoo/mcls/ima/IParamProvider.kt
@@ -1,5 +1,11 @@
 package tv.mycujoo.mcls.ima
 
+/**
+ * Contract to support custom parameter logging in IMA
+ */
 interface IParamProvider {
+    /**
+     * Map of key-value for logging custom
+     */
     fun params(): Map<String, String>
 }
