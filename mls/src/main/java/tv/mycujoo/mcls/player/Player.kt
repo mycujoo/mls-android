6eb310724e7427e8e30b16ef4ae8f925af835f4c
Shervinox <shervin@mycujoo.tv>
2021-06-02 14:57:45 +0200

    Document main Player component

--- mls/src/main/java/tv/mycujoo/mcls/player/Player.kt
+++ mls/src/main/java/tv/mycujoo/mcls/player/Player.kt
@@ -12,22 +12,80 @@ import tv.mycujoo.mcls.enum.C.Companion.DRM_WIDEVINE
 import tv.mycujoo.mcls.ima.IIma
 import tv.mycujoo.mcls.ima.ImaCustomParams
 
+/**
+ * MLS video player, implementing IPlayer contract.
+ * All video playing related functionality is done by this class
+ * @see IPlayer
+ */
 class Player : IPlayer {
 
+    /**region Fields*/
+    /**
+     * Exoplayer instance
+     */
     private var exoPlayer: SimpleExoPlayer? = null
+
+    /**
+     * IIma integration
+     * can be null, if IMA module is not used
+     */
     private var ima: IIma? = null
+
+    /**
+     * Mediafactory used to create playable item to feed ExoPlayer
+     */
     private lateinit var mediaFactory: MediaFactory
+
+    /**
+     * Handler to offload process from main thread to avoid UI blockage
+     */
     private lateinit var handler: Handler
+
+    /**
+     * Callback for processing media items after they are loaded by Exoplayer,
+     * meaning their tags are ready to be parsed.
+     */
     private lateinit var mediaOnLoadCompletedListener: MediaOnLoadCompletedListener
 
+    /**
+     * Latest resume position at playing, if video player is playing.
+     * This value is invalid if there is no item being played
+     */
     private var resumePosition: Long = C.INDEX_UNSET.toLong()
+
+    /**
+     * Latest window of playing, if video player is playing.
+     * This value is invalid if there is no item being played
+     */
     private var resumeWindow: Int = C.INDEX_UNSET
 
+    /**
+     * Indicator that shows player should automatically start playing the item,
+     * as soon as it's loaded
+     */
     private var playWhenReady: Boolean = false
+
+    /**
+     * Current playback position, if video player is playing.
+     * This value is invalid if there is no item being played
+     */
     private var playbackPosition: Long = -1L
 
+    /**
+     * information of the item being played
+     */
     private var mediaData: MediaDatum? = null
+    /**endregion */
 
+
+    /**
+     * Create a ready-to-use Player by setting all the given properties
+     * @param ima IMA integration, if Ima module is used
+     * @param mediaFactory factory for creating media items
+     * @param exoPlayer exoplayer used for playing video
+     * @param handler handler for running jobs on other thread than Main thread
+     * @param mediaOnLoadCompletedListener callback for sending data after media item is loaded by exoplayer
+     */
     override fun create(
         ima: IIma?,
         mediaFactory: MediaFactory,
@@ -42,36 +100,67 @@ class Player : IPlayer {
         this.mediaOnLoadCompletedListener = mediaOnLoadCompletedListener
     }
 
+    /**
+     * re-initialize Player by setting those properties that are life-cycle bound
+     * @param exoPlayer exoplayer used for playing video
+     * @param mediaOnLoadCompletedListener callback for sending data after media item is loaded by exoplayer
+     */
     override fun reInit(exoPlayer: SimpleExoPlayer) {
         this.exoPlayer = exoPlayer
         this.mediaOnLoadCompletedListener = MediaOnLoadCompletedListener(exoPlayer)
 
     }
 
+    /**
+     * @return true if player is ready to play, false otherwise
+     */
     override fun isReady(): Boolean {
         return exoPlayer != null
     }
 
+    /**
+     * @return Exoplayer
+     * Should be removed
+     */
     override fun getDirectInstance(): ExoPlayer? {
         return exoPlayer
     }
 
+    /**
+     * @return Exoplayer
+     */
     override fun getPlayer(): Player? {
         return exoPlayer
     }
 
+    /**
+     * Add listener to Exoplayer
+     * @param eventListener implementation of EventListener
+     */
     override fun addListener(eventListener: Player.EventListener) {
         exoPlayer?.addListener(eventListener)
     }
 
+    /**
+     * Seek to given offset
+     * @param offset
+     */
     override fun seekTo(offset: Long) {
         exoPlayer?.seekTo(offset)
     }
 
+    /**
+     * Current position
+     * @return current position if a media item has been loaded, or invalid otherwise
+     */
     override fun currentPosition(): Long {
         return exoPlayer?.currentPosition ?: -1L
     }
 
+    /**
+     * Current absolute time
+     * @return current absolute time if a media item has been loaded, or invalid otherwise
+     */
     override fun currentAbsoluteTime(): Long {
         exoPlayer?.let { exoplayer ->
             val dvrWindowStartTime = mediaOnLoadCompletedListener.getWindowStartTime()
@@ -84,10 +173,18 @@ class Player : IPlayer {
         return -1L
     }
 
+    /**
+     * Duration of current media item
+     * @return duration of current media item, or invalid otherwise
+     */
     override fun duration(): Long {
         return exoPlayer?.duration ?: -1L
     }
 
+    /**
+     * Live status of current media item
+     * @return true of it is live, or false otherwise
+     */
     override fun isLive(): Boolean {
         exoPlayer?.let {
             return (it.isCurrentWindowDynamic && it.contentPosition != 0L)
@@ -95,6 +192,10 @@ class Player : IPlayer {
         return false
     }
 
+    /**
+     * Playing status of current media item
+     * @return true of it is playing media item, or false otherwise
+     */
     override fun isPlaying(): Boolean {
         exoPlayer?.let {
             return it.isPlaying
@@ -102,6 +203,10 @@ class Player : IPlayer {
         return false
     }
 
+    /**
+     * Playing-Ad status of current media item
+     * @return true of it is playing an Ad item, or false otherwise
+     */
     override fun isPlayingAd(): Boolean {
         exoPlayer?.let {
             return it.isPlayingAd
@@ -109,6 +214,10 @@ class Player : IPlayer {
         return false
     }
 
+    /**
+     * Update resume position
+     * Ensures latest position is retrievable after recreation
+     */
     private fun updateResumePosition() {
         if (exoPlayer == null) {
             return
@@ -120,6 +229,9 @@ class Player : IPlayer {
         ) else C.POSITION_UNSET.toLong()
     }
 
+    /**
+     * Release resources
+     */
     override fun release() {
         exoPlayer?.let {
             updateResumePosition()
@@ -131,6 +243,10 @@ class Player : IPlayer {
         }
     }
 
+    /**
+     * reset all values to default
+     * Should only be called in onDestroy
+     */
     override fun destroy() {
         resumePosition = C.INDEX_UNSET.toLong()
         resumeWindow = C.INDEX_UNSET
@@ -139,6 +255,10 @@ class Player : IPlayer {
         mediaData = null
     }
 
+    /**
+     * Play media item
+     * @param drmMediaData
+     */
     override fun play(drmMediaData: MediaDatum.DRMMediaData) {
         this.mediaData = drmMediaData
 
@@ -151,12 +271,21 @@ class Player : IPlayer {
         play(mediaItem, drmMediaData.autoPlay)
     }
 
+    /**
+     * Play media item
+     * @param mediaData
+     */
     override fun play(mediaData: MediaDatum.MediaData) {
         this.mediaData = mediaData
         val mediaItem = mediaFactory.createMediaItem(mediaData.fullUrl)
         play(mediaItem, mediaData.autoPlay)
     }
 
+    /**
+     * Play media item
+     * @param mediaItem
+     * @param autoPlay
+     */
     private fun play(mediaItem: MediaItem, autoPlay: Boolean) {
         if (playbackPosition != -1L) {
             exoPlayer!!.seekTo(playbackPosition)
@@ -221,18 +350,27 @@ class Player : IPlayer {
 
     }
 
+    /**
+     * Play current media item
+     */
     override fun play() {
         exoPlayer?.let {
             it.playWhenReady = true
         }
     }
 
+    /**
+     * Pause current media item
+     */
     override fun pause() {
         exoPlayer?.let {
             it.playWhenReady = false
         }
     }
 
+    /**
+     * Load last loaded media item
+     */
     override fun loadLastVideo() {
         mediaData?.let {
             when (mediaData) {
@@ -249,6 +387,10 @@ class Player : IPlayer {
         }
     }
 
+    /**
+     * Compare absolute time with current segment time range
+     * @param targetAbsoluteTime any time in absolute time system
+     */
     override fun isWithinValidSegment(targetAbsoluteTime: Long): Boolean? {
         if (exoPlayer == null) {
             return null
@@ -261,11 +403,19 @@ class Player : IPlayer {
     }
 
 
+    /**
+     * Return current DVR window size
+     * @return dvr-window-size of current media item, if it is loaded. Invalid otherwise
+     */
     override fun dvrWindowSize(): Long {
         return mediaData?.dvrWindowSize ?: -1L
 
     }
 
+    /**
+     * Return current DVR window start time
+     * @return dvr-window start time of current media item, if it is loaded. Invalid otherwise
+     */
     override fun dvrWindowStartTime(): Long {
         exoPlayer?.let { _ ->
             return mediaOnLoadCompletedListener.getWindowStartTime()
