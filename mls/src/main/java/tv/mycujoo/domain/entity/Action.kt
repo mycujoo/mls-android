7570c6998704403f88200d2461a3d3f8c6982b10
Shervinox <shervin@mycujoo.tv>
2021-06-02 16:50:21 +0200

    Document Action.kt

--- mls/src/main/java/tv/mycujoo/domain/entity/Action.kt
+++ mls/src/main/java/tv/mycujoo/domain/entity/Action.kt
@@ -5,6 +5,14 @@ import tv.mycujoo.mcls.model.ScreenTimerDirection
 import tv.mycujoo.mcls.model.ScreenTimerFormat
 import java.util.*
 
+/**
+ * Annotation Action base sealed class.
+ * All annotation action will be of this type.
+ * @property id identifier for action
+ * @property offset the offset that action should happen at
+ * @property absoluteTime the absolute time that action should happen at
+ * @property priority priority of the action, compared to other actions
+ */
 sealed class Action {
     /**region Abstract fields*/
     abstract val id: String
@@ -15,18 +23,36 @@ sealed class Action {
 
     /**region Abstract functions*/
     abstract fun updateOffset(newOffset: Long): Action
+
+    /**
+     * return eligibility of action at given offset.
+     * Defaults to true
+     */
     open fun isEligible(): Boolean {
         return true
     }
 
     /**endregion */
 
+    /**
+     * Determines if an action belongs to this moment or is within this moment and next 1000ms
+     */
     fun isTillNowOrInRange(currentTime: Long): Boolean {
         return currentTime + C.ONE_SECOND_IN_MS > offset
     }
 
 
     /**region Overlay related*/
+    /**
+     * An Annotation action which requires SDK to show an overlay.
+     * @property svgData data needed to parse SVG into overlay
+     * @property duration duration of current action
+     * @property viewSpec position and size of required overlay
+     * @property introTransitionSpec intro transition that should be displayed while bringing the overlay into screen
+     * @property outroTransitionSpec outro transition that should be displayed while moving the overlay out of screen
+     * @property placeHolders variable placeholder in this overlay
+     * @property customId custom identifier of overlay
+     */
     data class ShowOverlayAction(
         override val id: String,
         override var offset: Long,
@@ -42,7 +68,7 @@ sealed class Action {
         override val priority: Int = 0
 
         override fun updateOffset(newOffset: Long): ShowOverlayAction {
-            if (offset == -1L){
+            if (offset == -1L) {
                 return this
             }
             var newIntroTransitionSpec: TransitionSpec? = null
@@ -75,7 +101,11 @@ sealed class Action {
         }
     }
 
-
+    /**
+     * An Annotation action which requires SDK to remove an existing overlay.
+     * @property outroTransitionSpec outro transition that should be displayed while moving the overlay out of screen
+     * @property customId custom identifier of overlay
+     */
     data class HideOverlayAction(
         override val id: String,
         override var offset: Long,
@@ -101,7 +131,10 @@ sealed class Action {
         }
     }
 
-
+    /**
+     * An Annotation action which requires SDK to Re-Show an overlay.
+     * @property customId custom identifier of overlay
+     */
     data class ReshowOverlayAction(
         override val id: String,
         override var offset: Long,
@@ -122,6 +155,16 @@ sealed class Action {
     /**endregion */
 
     /**region Timer related*/
+    /**
+     * Action to create a Timer
+     * @property name name of timer
+     * @property format format of time, MM:SS or just SS
+     * @property direction time is decreased or increased
+     * @property startValue start value of timer
+     * @property capValue max value that timer considers valid
+     * @see ScreenTimerFormat
+     * @see ScreenTimerDirection
+     */
     data class CreateTimerAction(
         override val id: String,
         override var offset: Long,
@@ -148,6 +191,10 @@ sealed class Action {
         }
     }
 
+    /**
+     * Action to start a Timer
+     * @property name name of the timer that should be started
+     */
     data class StartTimerAction(
         override val id: String,
         override var offset: Long,
@@ -166,6 +213,10 @@ sealed class Action {
         }
     }
 
+    /**
+     * Action to pause a Timer
+     * @property name name of the timer that should be started
+     */
     data class PauseTimerAction(
         override val id: String,
         override var offset: Long,
@@ -184,6 +235,11 @@ sealed class Action {
         }
     }
 
+    /**
+     * Action to adjust value of a Timer
+     * @property name name of the timer that should be started
+     * @property value amount of adjustment
+     */
     data class AdjustTimerAction(
         override val id: String,
         override var offset: Long,
@@ -204,6 +260,11 @@ sealed class Action {
         }
     }
 
+    /**
+     * Action to skip value of a Timer
+     * @property name name of the timer that should be started
+     * @property value amount of skip
+     */
     data class SkipTimerAction(
         override val id: String,
         override var offset: Long,
@@ -226,6 +287,10 @@ sealed class Action {
     /**endregion */
 
     /**region Variable related*/
+    /**
+     * Action to create a variable to be used in overlays
+     * @property variable
+     */
     data class CreateVariableAction(
         override val id: String,
         override var offset: Long,
@@ -243,6 +308,11 @@ sealed class Action {
         }
     }
 
+    /**
+     * Action to increase a variable's value
+     * @property name name of target variable
+     * @property amount amount to increase
+     */
     data class IncrementVariableAction(
         override val id: String,
         override var offset: Long,
@@ -264,6 +334,12 @@ sealed class Action {
     /**endregion */
 
     /**region Timeline-marker related*/
+    /**
+     * Action to mark a point in time-line as indicator of an event
+     * @property seekOffset possible seeking offset
+     * @property label label on top of marker
+     * @property color color of marker
+     */
     data class MarkTimelineAction(
         override val id: String,
         override var offset: Long,
@@ -274,7 +350,7 @@ sealed class Action {
     ) : Action() {
         override val priority: Int = 0
         override fun updateOffset(newOffset: Long): MarkTimelineAction {
-            if (offset == -1L){
+            if (offset == -1L) {
                 return this
             }
             return MarkTimelineAction(
@@ -290,6 +366,10 @@ sealed class Action {
     /**endregion */
 
     /**region Other actions*/
+    /**
+     * Action which delete(ignore) another action
+     * @param targetActionId target action id that should be deleted
+     */
     data class DeleteAction(
         override val id: String,
         override var offset: Long,
@@ -307,6 +387,9 @@ sealed class Action {
         }
     }
 
+    /**
+     * Any action that is not in correct format, and must ignored will be an Invalid action
+     */
     data class InvalidAction(
         override val id: String,
         override var offset: Long,
@@ -314,7 +397,7 @@ sealed class Action {
     ) : Action() {
         override val priority: Int = 0
         override fun updateOffset(newOffset: Long): InvalidAction {
-            if (offset == -1L){
+            if (offset == -1L) {
                 return this
             }
             return InvalidAction(id = id, offset = newOffset, absoluteTime = absoluteTime)
