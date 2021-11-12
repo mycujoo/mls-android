bdfc6aa48e2dadefc56e2786944e928649acd08d
Mohammad Al Kalaleeb <mohammadalkalaleeb@mycujoo.tv>
2021-11-12 21:04:39 +0100

    Adding TV Activity to the app

--- app/src/main/java/tv/mycujoo/mlsapp/activity/TvMainActivity.kt
+++ app/src/main/java/tv/mycujoo/mlsapp/activity/TvMainActivity.kt
@@ -1,15 +1,18 @@
 package tv.mycujoo.mlsapp.activity
 
+import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import tv.mycujoo.mlsapp.R
 import tv.mycujoo.mlsapp.databinding.ActivityTvMainBinding
 
-class TvMainActivity : AppCompatActivity() {
+class TvMainActivity : Activity() {
     lateinit var uiBinding: ActivityTvMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         uiBinding = ActivityTvMainBinding.inflate(layoutInflater)
         setContentView(uiBinding.root)
     }
