03bc3a924462a409e223c9d3ba0c1bdb6eda44ed
Mohammad Al Kalaleeb <mohammadalkalaleeb@mycujoo.tv>
2021-11-17 12:31:09 +0100

    Added test functions to MainActivity.kt to use Event Entity

--- app/src/main/java/tv/mycujoo/mlsapp/activity/MainActivity.kt
+++ app/src/main/java/tv/mycujoo/mlsapp/activity/MainActivity.kt
@@ -8,9 +8,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
+import org.joda.time.DateTime
 import tv.mycujoo.domain.entity.EventEntity
 import tv.mycujoo.domain.entity.EventStatus
 import tv.mycujoo.domain.entity.OrderByEventsParam
+import tv.mycujoo.domain.entity.Stream
 import tv.mycujoo.mcls.api.MLS
 import tv.mycujoo.mcls.api.MLSBuilder
 import tv.mycujoo.mcls.api.MLSConfiguration
@@ -83,10 +85,33 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
 
-
         activityMainBindings.mainActivityPlayButton.setOnClickListener {
             mMLS.getVideoPlayer().playVideo(MCLS_TEST_EVENT_ID)
         }
+
+        activityMainBindings.mainActivityPlayEventButton.setOnClickListener {
+            mMLS.getVideoPlayer().playVideo(
+                EventEntity(
+                    id = "ckvwbcerfqyub0hbqwbkyrvk1",
+                    title = "Brescia Calcio Femminile vs Pro Sesto Femminile",
+                    description = "Brescia Calcio Femminile vs Pro Sesto Femminile - Serie B Femminile",
+                    thumbnailUrl = "https://m-tv.imgix.net/07d68ec5a2469a64ef15e3a9c381bd1ff89b8d08b0ea0271fe3067361c6743df.jpg",
+                    poster_url = null,
+                    location = null,
+                    organiser = null,
+                    start_time = DateTime.parse("2021-11-14T14:30:00.000+01:00"),
+                    status = EventStatus.EVENT_STATUS_FINISHED,
+                    streams = listOf(),
+                    timezone = null,
+                    timeline_ids = listOf(),
+                    metadata = null,
+                    is_test = false,
+                    isNativeMLS = false
+                )
+            )
+        }
+
+
         activityMainBindings.mainActivityPlayButton2.setOnClickListener {
             mMLS.getDataProvider().fetchEvents(
                 pageSize = 10,
