72df0e3395535263e7ff4f6d77fc6798450e16f1
Mohammad Al Kalaleeb <mohammadalkalaleeb@mycujoo.tv>
2021-11-17 15:39:37 +0100

    removing unused class

--- mls/src/main/java/tv/mycujoo/mcls/MissingKeyException.kt
+++
@@ -1,9 +0,0 @@
-package tv.mycujoo.mcls
-
-import java.lang.Exception
-
-class MissingKeyException : Exception() {
-    override fun toString(): String {
-        return "Missing Key, Please see https://github.com/mycujoo/mls-android"
-    }
-}
