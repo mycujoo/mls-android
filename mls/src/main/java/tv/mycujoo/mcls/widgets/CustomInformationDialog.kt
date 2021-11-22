d62be6f45e4000afb760c61f1c9c144785691fd4
Mohammad Al Kalaleeb <mohammadalkalaleeb@mycujoo.tv>
2021-11-22 11:39:49 +0100

    Fixing disappearing dialogs after more than one error

--- mls/src/main/java/tv/mycujoo/mcls/widgets/CustomInformationDialog.kt
+++ mls/src/main/java/tv/mycujoo/mcls/widgets/CustomInformationDialog.kt
@@ -3,12 +3,10 @@ package tv.mycujoo.mcls.widgets
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatImageView
-import kotlinx.android.synthetic.main.dialog_event_info_pre_event_layout.view.*
 import tv.mycujoo.mcls.R
 
 /**
@@ -25,19 +23,15 @@ class CustomInformationDialog(
     init {
         LayoutInflater.from(context)
             .inflate(R.layout.dialog_event_info_pre_event_layout, this, true)
+
         container.addView(this)
 
-        container.findViewById<LinearLayout>(R.id.preEventInfoDialog_textualLayout).visibility =
-            VISIBLE
-        container.findViewById<AppCompatImageView>(R.id.eventInfoPreEventDialog_posterView).visibility =
-            GONE
+        this.findViewById<View>(R.id.preEventInfoDialog_textualLayout).visibility = VISIBLE
+        this.findViewById<View>(R.id.eventInfoPreEventDialog_posterView).visibility = GONE
 
-        container.findViewById<TextView>(R.id.preEventInfoDialog_titleTextView).text =
-            uiEvent.title ?: ""
-        container.findViewById<TextView>(R.id.preEventInfoDialog_bodyTextView).text = message
-        container.findViewById<TextView>(R.id.preEventInfoDialog_bodyTextView)
-            .setTextColor(Color.RED)
-        container.findViewById<TextView>(R.id.preEventInfoDialog_startTimeTextView).visibility =
-            GONE
+        this.findViewById<TextView>(R.id.preEventInfoDialog_titleTextView).text = uiEvent.title ?: ""
+        this.findViewById<TextView>(R.id.preEventInfoDialog_bodyTextView).text = message
+        this.findViewById<TextView>(R.id.preEventInfoDialog_bodyTextView).setTextColor(Color.RED)
+        this.findViewById<TextView>(R.id.preEventInfoDialog_startTimeTextView).visibility = GONE
     }
 }
