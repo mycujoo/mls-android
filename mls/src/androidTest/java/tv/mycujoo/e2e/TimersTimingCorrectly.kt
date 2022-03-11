527acb5d8ef50a2d5d8bdb705b396a80c5bc4c3a
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2022-03-11 10:20:12 +0100

    Mapping AdjustTimer correctly Cleaning data init from the test function

--- mls/src/androidTest/java/tv/mycujoo/e2e/TimersTimingCorrectly.kt
+++ mls/src/androidTest/java/tv/mycujoo/e2e/TimersTimingCorrectly.kt
@@ -1,65 +1,94 @@
 package tv.mycujoo.e2e
 
+import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.idling.CountingIdlingResource
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.amshove.kluent.shouldBeEqualTo
 import org.joda.time.DateTime
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import tv.mycujoo.E2ETest
 import tv.mycujoo.IdlingResourceHelper
 import tv.mycujoo.domain.entity.*
+import tv.mycujoo.mcls.di.PlayerModule
+import tv.mycujoo.mcls.manager.VariableKeeper
 import tv.mycujoo.mcls.model.ScreenTimerDirection
 import tv.mycujoo.mcls.model.ScreenTimerFormat
+import javax.inject.Inject
 
 @HiltAndroidTest
+@UninstallModules(PlayerModule::class)
 class TimersTimingCorrectly : E2ETest() {
 
-    private val videoIdlingResource = CountingIdlingResource("VIDEO")
+    @BindValue
+    val context: Context = InstrumentationRegistry.getInstrumentation().context
 
+    @BindValue
+    val exoPlayer = ExoPlayer.Builder(context).build()
+
+    @Inject
+    lateinit var variableKeeper: VariableKeeper
+
+    private val videoIdlingResource = CountingIdlingResource("VIDEO")
+    private val warmupResource = CountingIdlingResource("WARM_UP")
     val helper = IdlingResourceHelper(videoIdlingResource)
+
+    @Before
+    fun setUp() {
+        IdlingRegistry.getInstance().register(videoIdlingResource)
+        IdlingRegistry.getInstance().register(warmupResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(videoIdlingResource)
+        IdlingRegistry.getInstance().unregister(warmupResource)
+    }
 
     @Test
     fun testTimerAdjustment() {
-        val event = EventEntity(
-            id = "ckw25ntnkxlam0hbqnfhx3gk0",
-            title = "Top 10 Gol Serie C 2021/22 - 14^ Giornata",
-            description = "I dieci gol più belli della 14^ giornata di Serie C",
-            thumbnailUrl = null,
-            poster_url = null,
-            location = null,
-            organiser = null,
-            start_time = DateTime.parse("2021-11-14T14:30:00.000+01:00"),
-            status = EventStatus.EVENT_STATUS_FINISHED,
-            streams = listOf(
-                Stream(
-                    id = "ckw25ntnkxlam0hbqnfhx3gk0",
-                    dvrWindowString = null,
-                    fullUrl = "https://europe-west-hls.mls.mycujoo.tv/mohammadalkalaleeb/ckwwahs2o000k0123hr2pqqwe/master.m3u8",
-                    widevine = null,
-                    errorCodeAndMessage = null
-                )
-            ),
-            timezone = null,
-            timeline_ids = listOf(),
-            metadata = null,
-            is_test = false,
-            isNativeMLS = false
-        )
+        videoIdlingResource.increment()
+        warmupResource.increment()
+
+        exoPlayer.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                if (isPlaying && !videoIdlingResource.isIdleNow) videoIdlingResource.decrement()
+            }
+        })
 
         UiThreadStatement.runOnUiThread {
-            mMLS.getVideoPlayer().playVideo(event)
+            mMLS.getVideoPlayer().playVideo(sampleEvent)
 
             // To Evade ExoPlayer OnMediaTransition Clearing Actions, We Wait a couple seconds
             Handler(Looper.getMainLooper()).postDelayed({
                 val actions = actionSet1()
 
                 mMLS.getVideoPlayer().setLocalAnnotations(actions)
-            }, 2000)
+            }, 1000)
         }
 
-        Thread.sleep(25000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // TODO: Improve This, this doesn't match correct behavior
+            variableKeeper.observeOnTimer("\$${TIMER_NAME}") {
+                it.second shouldBeEqualTo "1:00"
+                warmupResource.decrement()
+            }
+        }, 10000)
+
+        // To make sure the test has passed
+        Thread.sleep(11000)
     }
 
     companion object {
@@ -78,66 +107,30 @@ class TimersTimingCorrectly : E2ETest() {
                 listOf(
                     Action.StartTimerAction(
                         id = "timer",
-                        offset = 0,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer"
-                    ),
-                    Action.PauseTimerAction(
-                        id = "timer",
-                        offset = 250,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer"
-                    ),
-                    Action.StartTimerAction(
-                        id = "timer",
-                        offset = 500,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer",
-                    ),
-                    Action.PauseTimerAction(
-                        id = "timer",
-                        offset = 750,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer"
-                    ),
-                    Action.StartTimerAction(
-                        id = "timer",
                         offset = 1000,
                         absoluteTime = INVALID_TIME,
                         name = "\$main_timer"
                     ),
                     Action.PauseTimerAction(
                         id = "timer",
-                        offset = 1250,
+                        offset = 6000,
                         absoluteTime = INVALID_TIME,
                         name = "\$main_timer"
                     ),
                     // This is causing trouble if the timer is paused
                     Action.AdjustTimerAction(
                         id = "timer",
-                        offset = 1450,
+                        offset = 7000,
                         absoluteTime = INVALID_TIME,
                         name = "\$main_timer",
-                        value = 50000
+                        value = 60000
                     ),
                     Action.StartTimerAction(
                         id = "timer",
-                        offset = 1500,
+                        offset = 7500,
                         absoluteTime = INVALID_TIME,
                         name = "\$main_timer",
                     ),
-                    Action.PauseTimerAction(
-                        id = "timer",
-                        offset = 1750,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer"
-                    ),
-                    Action.StartTimerAction(
-                        id = "timer",
-                        offset = 2000,
-                        absoluteTime = INVALID_TIME,
-                        name = "\$main_timer",
-                    )
                 )
             )
 
@@ -166,6 +159,32 @@ class TimersTimingCorrectly : E2ETest() {
 
             return actions
         }
+
+        val sampleEvent = EventEntity(
+            id = "ckw25ntnkxlam0hbqnfhx3gk0",
+            title = "Top 10 Gol Serie C 2021/22 - 14^ Giornata",
+            description = "I dieci gol più belli della 14^ giornata di Serie C",
+            thumbnailUrl = null,
+            poster_url = null,
+            location = null,
+            organiser = null,
+            start_time = DateTime.parse("2021-11-14T14:30:00.000+01:00"),
+            status = EventStatus.EVENT_STATUS_FINISHED,
+            streams = listOf(
+                Stream(
+                    id = "ckw25ntnkxlam0hbqnfhx3gk0",
+                    dvrWindowString = null,
+                    fullUrl = "https://europe-west-hls.mls.mycujoo.tv/mohammadalkalaleeb/ckwwahs2o000k0123hr2pqqwe/master.m3u8",
+                    widevine = null,
+                    errorCodeAndMessage = null
+                )
+            ),
+            timezone = null,
+            timeline_ids = listOf(),
+            metadata = null,
+            is_test = false,
+            isNativeMLS = false
+        )
 
         private fun footballDefaultActions(
             homeTeamAbbr: String = "HOME",
