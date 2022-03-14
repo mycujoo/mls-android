22b492edad4c77c436bd70c7775ab7636fbfda78
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2022-03-14 14:56:18 +0100

    Moving Socket to separate module for testability

---
+++ mls/src/main/java/tv/mycujoo/mcls/di/NetworkModuleBinds.kt
@@ -0,0 +1,23 @@
+package tv.mycujoo.mcls.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import tv.mycujoo.mcls.network.socket.ConcurrencySocket
+import tv.mycujoo.mcls.network.socket.IConcurrencySocket
+import tv.mycujoo.mcls.network.socket.IReactorSocket
+import tv.mycujoo.mcls.network.socket.ReactorSocket
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+interface NetworkModuleBinds {
+    @Binds
+    @Singleton
+    fun bindReactorSocket(ReactorSocket: ReactorSocket): IReactorSocket
+
+    @Binds
+    @Singleton
+    fun bindConcurrencySocket(concurrencySocket: ConcurrencySocket): IConcurrencySocket
+}
