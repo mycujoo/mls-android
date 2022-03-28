7f5bf7ac7ad0fd9a90f4c8f46dd3324a2944241c
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2022-03-28 13:40:12 +0200

    Added Crash Checks for Force Connection Termination

--- mls/src/test/java/tv/mycujoo/mcls/network/socket/BFFRTSocketTest.kt
+++ mls/src/test/java/tv/mycujoo/mcls/network/socket/BFFRTSocketTest.kt
@@ -21,14 +21,13 @@ import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.*
 import tv.mycujoo.mcls.utils.ThreadUtils
 import tv.mycujoo.mcls.utils.UserPreferencesUtils
+import java.lang.Exception
 
 @RunWith(MockitoJUnitRunner::class)
 class BFFRTSocketTest {
 
     private lateinit var mainWebSocketListener: MainWebSocketListener
     private lateinit var mBFFRTSocket: BFFRTSocket
-
-    private val mockWebServer = MockWebServer()
 
     @Mock
     lateinit var okHttpClient: OkHttpClient
@@ -39,9 +38,10 @@ class BFFRTSocketTest {
     @Mock
     lateinit var userPreferencesUtils: UserPreferencesUtils
 
+    private val MOCK_WEB_SERVER_PORT = 8004
+
     @Before
     fun setup() {
-        mockWebServer.start()
         mainWebSocketListener = MainWebSocketListener()
         whenever(okHttpClient.newWebSocket(any(), any())).thenReturn(webSocket)
 
@@ -50,14 +50,9 @@ class BFFRTSocketTest {
                 okHttpClient,
                 mainWebSocketListener,
                 userPreferencesUtils,
-                mockWebServer.url("/").toString(),
+                "http://localhost:$MOCK_WEB_SERVER_PORT/",
                 ThreadUtils()
             )
-    }
-
-    @After
-    fun tearDown() {
-        mockWebServer.shutdown()
     }
 
     @Test
@@ -91,6 +86,8 @@ class BFFRTSocketTest {
 
     @Test
     fun `given leaveCurrentSession should close network`(): Unit = runBlocking {
+        val mockWebServer = MockWebServer()
+        mockWebServer.start(MOCK_WEB_SERVER_PORT)
         whenever(userPreferencesUtils.getPseudoUserId()).thenReturn("pseudo_user_id")
         var socketOpened: Boolean? = null
 
@@ -153,5 +150,41 @@ class BFFRTSocketTest {
         delay(50)
 
         socketOpened shouldBeEqualTo false
+
+        mockWebServer.shutdown()
     }
+
+    @Test
+    fun `given leaveCurrentSession did not get close network from the server, it should force close`(): Unit =
+        runBlocking {
+            val mockWebServer = MockWebServer()
+            mockWebServer.start(MOCK_WEB_SERVER_PORT)
+
+            whenever(userPreferencesUtils.getPseudoUserId()).thenReturn("pseudo_user_id")
+
+            mockWebServer.dispatcher = object : Dispatcher() {
+                override fun dispatch(request: RecordedRequest): MockResponse {
+                    return MockResponse().withWebSocketUpgrade(object : WebSocketListener() {
+                    })
+                }
+            }
+
+            val bffRtSocket = BFFRTSocket(
+                okHttpClient = OkHttpClient(),
+                mainSocketListener = MainWebSocketListener(),
+                userPreferencesUtils = userPreferencesUtils,
+                webSocketUrl = mockWebServer.url("/").toString(),
+                ThreadUtils()
+            )
+
+            bffRtSocket.startSession("123", "id_token")
+            bffRtSocket.leaveCurrentSession()
+
+            delay(1000)
+
+            try {
+                mockWebServer.shutdown()
+            } catch (e: Exception) {
+            }
+        }
 }
